bf84fc5707de329732040f575abec46bb97d79bd
erayridvan <52295311+erayridvan@users.noreply.github.com>
2023-04-11 10:15:41 +0300

    Solved Task from Exam Tasks

---
+++ Exam09April2022/Exam-OOP-09-April-2022 OOPTasks/Formula1/Repositories/RaceRepository.cs
@@ -0,0 +1,40 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Formula1.Repositories
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        private List<IRace> models;
+
+        public RaceRepository()
+        {
+            models = new List<IRace>();
+        }
+
+        public IReadOnlyCollection<IRace> Models => models.AsReadOnly();
+
+        public void Add(IRace model)
+        {
+            models.Add(model);
+        }
+
+        public IRace FindByName(string name)
+        {
+            return models.FirstOrDefault(r => r.RaceName == name);
+        }
+
+        public bool Remove(IRace model)
+        {
+            if (models.Contains(model))
+            {
+                models.Remove(model);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Exam09April2022/Exam-OOP-09-April-2022 UnitTests/SmartphoneShop.Tests/SmartphoneShopTests.cs
@@ -0,0 +1,165 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SmartphoneShop.Tests
+{
+    [TestFixture]
+    public class SmartphoneShopTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Smartphone smartphone = new Smartphone("IPhone", 100);
+
+            Assert.AreEqual("IPhone", smartphone.ModelName);
+            Assert.AreEqual(100, smartphone.MaximumBatteryCharge);
+            Assert.AreEqual(100, smartphone.CurrentBateryCharge);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Shop shop = new Shop(100);
+
+            Assert.AreEqual(100, shop.Capacity);
+            Assert.AreEqual(0, shop.Count);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Shop shop = new Shop(-1);
+            });
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Smartphone smartphone = new Smartphone("IPhone", 100);
+            Shop shop = new Shop(100);
+
+            shop.Add(smartphone);
+
+            Assert.AreEqual(1,shop.Count);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            Smartphone smartphone = new Smartphone("IPhone", 100);
+            Smartphone smartphoneOne = new Smartphone("IPhone", 100);
+            Shop shop = new Shop(3);
+
+            shop.Add(smartphone);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.Add(smartphoneOne);
+            });
+        }
+
+        [Test]
+        public void Test6()
+        {
+            Smartphone smartphone = new Smartphone("IPhone", 100);
+            Smartphone smartphoneOne = new Smartphone("IPhone", 100);
+            Shop shop = new Shop(1);
+
+            shop.Add(smartphone);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.Add(smartphoneOne);
+            });
+        }
+
+        [Test]
+        public void Test7()
+        {
+            Shop shop = new Shop(1);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.Remove("IPhone");
+            });
+        }
+
+        [Test]
+        public void Test8()
+        {
+            Smartphone smartphone = new Smartphone("IPhone", 100);
+            Smartphone smartphoneOne = new Smartphone("IPhone1", 100);
+            Shop shop = new Shop(2);
+
+            shop.Add(smartphone);
+            shop.Add(smartphoneOne);
+
+            shop.Remove("IPhone");
+            Assert.AreEqual(1,shop.Count);
+
+            shop.Remove("IPhone1");
+            Assert.AreEqual(0, shop.Count);
+
+        }
+
+        [Test]
+        public void Test9()
+        {
+            Smartphone smartphone = new Smartphone("IPhone", 100);
+            Smartphone smartphoneOne = new Smartphone("IPhone1", 100);
+            Smartphone smartphoneTwo = new Smartphone("IPhone2", 50);
+
+            Shop shop = new Shop(3);
+
+            shop.Add(smartphone);
+            shop.Add(smartphoneOne);
+            shop.Add(smartphoneTwo);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.TestPhone("IPhoneasdasf", 50);
+            });
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.TestPhone("IPhone2", 100);
+            });
+
+            shop.TestPhone("IPhone", 10);
+
+            Assert.AreEqual(90, smartphone.CurrentBateryCharge);
+        }
+
+        [Test]
+        public void Test10()
+        {
+            Smartphone smartphone = new Smartphone("IPhone", 100);
+            Smartphone smartphoneOne = new Smartphone("IPhone1", 100);
+            Smartphone smartphoneTwo = new Smartphone("IPhone2", 50);
+
+            Shop shop = new Shop(3);
+
+            shop.Add(smartphone);
+            shop.Add(smartphoneOne);
+            shop.Add(smartphoneTwo);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.ChargePhone("IPhoneasdasf");
+            });
+
+            shop.TestPhone("IPhone", 90);
+            shop.ChargePhone("IPhone");
+
+            Assert.AreEqual(100, smartphone.CurrentBateryCharge);
+        }
+    }
+}
