b6b81bd56f1bd9bc047a421a45c73fd57398ebde
erayridvan <52295311+erayridvan@users.noreply.github.com>
2023-04-28 06:56:59 +0300

    Problem Solved From Exam Tasks at Softuni

---
+++ 10December2022/Models/Cocktails/Cocktail.cs
@@ -0,0 +1,64 @@
+﻿using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Utilities.Messages;
+using System;
+
+namespace ChristmasPastryShop.Models.Cocktails
+{
+    public abstract class Cocktail : ICocktail
+    {
+        private string name;
+        private string size;
+        private double price;
+
+        public Cocktail(string cocktailName, string size, double price)
+        {
+            Name = cocktailName;
+            Size= size;
+            Price = price;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException
+                        (ExceptionMessages.NameNullOrWhitespace);
+                }
+
+                name = value;
+            }
+        }
+
+        public string Size
+        {
+            get { return size; }
+            private set { size = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            private set
+            {
+                if (this.Size == "Middle")
+                {
+                    value = (value / 3) * 2;
+                }
+                else if (this.Size == "Small")
+                {
+                    value = value / 3;
+                }
+
+                price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Size}) - {Price:f2} lv";
+        }
+    }
+}
---
+++ 10December2022/Repositories/CocktailRepository.cs
@@ -0,0 +1,23 @@
+﻿using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System.Collections.Generic;
+
+namespace ChristmasPastryShop.Repositories
+{
+    public class CocktailRepository : IRepository<ICocktail>
+    {
+        private readonly List<ICocktail> cocktails;
+
+        public CocktailRepository()
+        {
+            this.cocktails = new List<ICocktail>();
+        }
+
+        public IReadOnlyCollection<ICocktail> Models => this.cocktails;
+
+        public void AddModel(ICocktail model)
+        {
+            this.cocktails.Add(model);
+        }
+    }
+}
